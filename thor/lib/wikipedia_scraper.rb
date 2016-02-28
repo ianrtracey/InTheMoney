90feab931e7f2358fb72c1da6b422b452ad81898
Ian Tracey <ianrtracey@gmail.com>
2016-02-28 01:25:35 -0800

    Adds example web scrapers

---
+++ thor/lib/wikipedia_scraper.rb
@@ -0,0 +1,43 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'fileutils'
+
+PAGES_DIR='data/pages'
+FileUtils.makedirs(PAGES_DIR)
+
+BASE_URL = 'https://en.wikipedia.org/wiki/List_of_Nobel_laureates/'
+
+# Get index of most recent script:
+index_page = Nokogiri::HTML(open(BASE_URL))
+# contract_pages = index_page.css("div.title").map{ |x| x.css("a") }.flatten.map{|x| x['href']}
+# contract_ids = contract_pages.map{ |x| x.split("/")[-1] }
+
+# puts "Crawling index from #{contract_ids.first} to #{contract_ids.last}"
+# contract_ids.each do |idx|
+#   url = "#{BASE_URL}/#{S_URL}#{idx}"
+#   puts "Getting #{url}"
+#   retries = 2
+#   begin
+#   resp = Net::HTTP.get_response(URI.parse(url))
+#   if resp.code.match('200')
+#     fname = "#{PAGES_DIR}/#{idx}"
+#     File.open(fname, 'w') do |ofile|
+#       ofile.write(resp.body)
+#     end
+#     puts "Saved to #{fname}"
+#   else
+#     puts "\tNot a valid page"
+#   end
+# rescue StandardError=>e
+#   puts "#{idx}: #{e}"
+#     if retries > 0 
+#       puts "Retrying #{retries.length} more times"
+#       retries -= 1
+#       sleep 40
+#       retry
+#     end  
+#   else
+#     sleep 0.5 + rand * 3.0
+#   end
+# end
